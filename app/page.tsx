092f8fc0656568dd79cdf4bd57f62375fa6a6141
Charles Bither <charles@charlesbither.com>
2025-12-30 12:25:01 -0800

    replace default home screen background

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,21 @@ import SkillsContent from '@/components/SkillsContent';
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ backgroundColor: 'black' }}>
       <div
-        className="absolute top-0 min-h-screen w-full overflow-hidden bg-cover bg-center"
-        style={{ backgroundImage: "url('/assets/seattleO.jpg')", zIndex: -1 }}>
+        className="min-h-screen w-full overflow-hidden bg-cover bg-center"
+        style={{ backgroundImage: "url('/assets/rain.jpg')", backgroundSize: 'cover' }}>
         {/* Dark Overlay */}
         <div className="absolute top-0 inset-0 bg-black/60" />
       </div>
-      <Navbar />
-      <HomeContent />
-      <AboutContent />
-      <SkillsContent />
-      <ProjectsContent />
-      <ContactContent />
+      <div className="absolute top-0 w-full">
+        <Navbar />
+        <HomeContent />
+        <AboutContent />
+        <SkillsContent />
+        <ProjectsContent />
+        <ContactContent />
+      </div>
     </div>
   );
 }
